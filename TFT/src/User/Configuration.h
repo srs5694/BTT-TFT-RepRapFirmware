0261cacb237841b5ab39e93abc90328990e08e32
Slava <ukrslava@yandex.ua>
2020-05-28 20:42:05 +0300

    Manual Leveling 0.4

--- TFT/src/User/Configuration.h
+++ TFT/src/User/Configuration.h
@@ -213,7 +213,7 @@
 #define LEVELING_POINT_3_Y         (Y_MAX_POS - 30)
 #define LEVELING_POINT_4_X         (X_MIN_POS + 30)
 #define LEVELING_POINT_4_Y         (Y_MAX_POS - 30)
-#define LEVELING_POINT_Z           0.2f  // Z-axis position when nozzle stays for leveling
+#define LEVELING_POINT_Z           0.4f  // Z-axis position when nozzle stays for leveling
 #define LEVELING_POINT_MOVE_Z      10.0f // Z-axis position when nozzle move to next point
 #define LEVELING_POINT_XY_FEEDRATE 6000  // (mm/min) X and Y axes move feedrate
 #define LEVELING_POINT_Z_FEEDRATE  600   // (mm/min) Z axis move feedrate
